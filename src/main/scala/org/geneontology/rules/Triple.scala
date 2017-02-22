087006b5764e813a4365aaaf1ed5865ff7115c27
Jim Balhoff <jim@balhoff.org>
2017-02-22 14:09:09 -0500

    Removed unused functions.

--- src/main/scala/org/geneontology/rules/Triple.scala
+++ src/main/scala/org/geneontology/rules/Triple.scala
@@ -32,18 +32,14 @@ sealed trait TripleLike {
 
 final case class TriplePattern(s: Node, p: Node, o: Node) extends TripleLike {
 
-  private val subjectGetter: Triple => Node = (triple: Triple) => triple.s
-  private val predicateGetter: Triple => Node = (triple: Triple) => triple.p
-  private val objectGetter: Triple => Node = (triple: Triple) => triple.o
-
   def variables: Map[Variable, Set[TestSpecField]] = {
-    val sFunction: Map[Variable, Set[TestSpecField]] =
+    val subjectVariable: Map[Variable, Set[TestSpecField]] =
       if (this.s.isInstanceOf[Variable]) Map(this.s.asInstanceOf[Variable] -> Set(SubjectField)) else Map.empty
-    val pFunction: Map[Variable, Set[TestSpecField]] =
+    val predicateVariable: Map[Variable, Set[TestSpecField]] =
       if (this.p.isInstanceOf[Variable]) Map(this.p.asInstanceOf[Variable] -> Set(PredicateField)) else Map.empty
-    val oFunction: Map[Variable, Set[TestSpecField]] =
+    val objectVariable: Map[Variable, Set[TestSpecField]] =
       if (this.o.isInstanceOf[Variable]) Map(this.o.asInstanceOf[Variable] -> Set(ObjectField)) else Map.empty
-    sFunction |+| pFunction |+| oFunction //TODO rename
+    subjectVariable |+| predicateVariable |+| objectVariable
   }
 
 }
