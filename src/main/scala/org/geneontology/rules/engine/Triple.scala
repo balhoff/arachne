cf769615c284a65a48c61c14f33f79f8b84d9978
Jim Balhoff <jim@balhoff.org>
2017-05-23 21:19:20 -0400

    Added toString methods for triples and rules.

--- src/main/scala/org/geneontology/rules/engine/Triple.scala
+++ src/main/scala/org/geneontology/rules/engine/Triple.scala
@@ -10,15 +10,32 @@ final case class URI(uri: String) extends Resource {
 
   override val hashCode: Int = uri.hashCode
 
+  override def toString(): String = s"<$uri>"
+
 }
 
-final case class BlankNode(id: String) extends Resource
+final case class BlankNode(id: String) extends Resource {
 
-final case class Literal(lexicalForm: String, datatype: URI, lang: Option[String]) extends ConcreteNode
+  override def toString(): String = s"_:$id"
+
+}
+
+final case class Literal(lexicalForm: String, datatype: URI, lang: Option[String]) extends ConcreteNode {
+
+  override def toString(): String = {
+    val ln = lang.map(t => s"@$t").getOrElse("")
+    s""""$lexicalForm"^^$datatype$ln"""
+  }
+
+}
 
 sealed trait FluidNode extends Node
 
-final case class Variable(name: String) extends FluidNode
+final case class Variable(name: String) extends FluidNode {
+
+  override def toString(): String = name
+
+}
 
 final case object AnyNode extends FluidNode
 
@@ -27,6 +44,8 @@ sealed trait TripleLike {
   def s: Node
   def p: Node
   def o: Node
+
+  override def toString(): String = s"($s $p $o)"
 
 }
 
@@ -57,4 +76,8 @@ final case class Triple(s: Resource, p: URI, o: ConcreteNode) extends TripleLike
 
 }
 
-final case class Rule(name: Option[String], body: List[TriplePattern], head: List[TriplePattern])
+final case class Rule(name: Option[String], body: List[TriplePattern], head: List[TriplePattern]) {
+
+  override def toString(): String = s"[${name.getOrElse("")} ${body.mkString(" ^ ")} -> ${head.mkString(" ^ ")} ]"
+
+}
